75e26167efc9a636ed236f2033d00f8aa11b1104
maor <maor@BULLSEYE-debian-live-builder>
2021-08-18 09:23:10 +0300

    try to initiate node program with params 4.3.0 - return gap from node as output

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ const core = require('@actions/core');
 try{
     const mainTime = parseInt(process.argv[2])
     const devTime = parseInt(process.argv[3])
+    const allowedGap = parseInt(process.argv[4)
+    const gapSec = mainTime-devTime;
+    const gapHour = gapSec/3600;
+    const result = gapHour>=allowedGap
     // console.log(mainTime,devTime, mainTime-devTime)
-    core.setOutput("gap", mainTime-devTime);
+    core.setOutput("isExceededGap", result);
 
 }catch (e) {
     console.log(e)
